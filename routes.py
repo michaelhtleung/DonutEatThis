a2c1b254e3e51b4454e50ba980d14ee2ce198c3e
Denny <denlee973@gmail.com>
2021-03-06 16:25:06 -0500

    add upload image function and import Request from flask

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Request
 
 app = Flask(__name__)
 
@@ -6,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'goodbye, World!'
+
+@app.route('/uploadImage', methods=['GET','POST'])
+def upload_img():
+    if request.method == 'POST':
+        f = request.files['product_img']
+        f.save('/var/www/uploads/product_img.jpg')
+        return 'image uploaded!'
+    return 'image not uploaded...'
