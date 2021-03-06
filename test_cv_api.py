c5675e39f2460a60f3a99415c6c64de13fc48135
Michael <michaelhtleung@gmail.com>
2021-03-06 16:00:12 -0500

    Add CV pipeline, fail to remove french words

--- test_cv_api.py
+++ test_cv_api.py
@@ -1,25 +1,63 @@
-import io
 import os
 
-# Imports the Google Cloud client library
-from google.cloud import vision
+# [START vision_fulltext_detection]
+def detect_document(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+    # [START vision_python_migration_document_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
 
-# The name of the image file to annotate
-file_name = os.path.abspath('hashbrowns_ingredients.jpeg')
+    image = vision.Image(content=content)
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    response = client.document_text_detection(image=image)
+    result = []
 
-image = vision.Image(content=content)
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+                for word in paragraph.words:
+                    # remove words with any french
+                    if word.property:
+                        if word.property.detected_languages:
+                            for language in word.property.detected_languages:
+                                # breakpoint()
+                                if language.language_code:
+                                    # breakpoint()
+                                    if 'fr' == language.language_code:
+                                        # breakpoint()
+                                        continue
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    # print('Word text: {} (confidence: {})'.format(
+                    #     word_text, word.confidence))
+                    if word.confidence > 90:
+                        result.append(word_text)
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    # return response
+    return result
+    # [END vision_python_migration_document_text_detection]
+# [END vision_fulltext_detection]
+
+
+def test():
+    # The name of the image file to annotate
+    file_name = os.path.abspath('hashbrowns_ingredients.jpeg')
+    response = detect_document(path=file_name)
+    return response
+
+# file_name = os.path.abspath('hashbrowns_ingredients.jpeg')
+# response = detect_document(path=file_name)
