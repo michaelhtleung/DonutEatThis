a436ea2679fcf6e086b55fe0858e9a7dd8d8a82d
Denny <denlee973@gmail.com>
2021-03-06 21:14:13 -0500

    save image to uploads directory

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/uploads/'
+UPLOAD_FOLDER = 'uploads/'
 app = Flask(__name__)
 
 
@@ -16,7 +16,6 @@ def upload_img():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     if request.method == 'POST':
         img = request.files['product_img']
-        # img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
-        img.save(secure_filename(img.filename))
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
         return 'image uploaded!'
     return 'image not uploaded...'
