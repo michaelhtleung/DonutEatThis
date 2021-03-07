ca118ef7be7a5473f870312c33d3b57b44fe4321
Michael <michaelhtleung@gmail.com>
2021-03-07 07:21:27 -0500

    Append sentiment analysis to pipeline

--- test_sql.py
+++ test_sql.py
@@ -32,7 +32,7 @@ cursor.execute(query)
 subclass = None
 for (element) in cursor:
     element = element[0]
-    print(element)
+    # print(element)
     # if the first word is 21 letters long, that's probably not the one we want to keep
     if element != 'NULL' and element != 'None' and element != None and len(element.split(' ')[0]) < 20:
         subclass = element
@@ -46,13 +46,29 @@ cursor.execute(query)
 effects = []
 for (element) in cursor:
     element = element[0]
-    print(element)
+    # print(element)
     if element != 'NULL' and element != 'None' and element != None:
         effects.append(element)
-print(f'effects: {effects}')
+print(f'effects: {effects}') # list of str
 cursor.close()
 
-
+# Sentiment Analysis:
+from google.cloud import language_v1
+# Instantiates a client
+client = language_v1.LanguageServiceClient()
+effects = ''.join(effects)
+document = language_v1.Document(content=effects, type_=language_v1.Document.Type.PLAIN_TEXT)
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+# print("Text: {}".format(effects))
+# print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+is_healthy = None
+if sentiment.score >= 0:
+    is_healthy = True
+else:
+    is_healthy = False
+print(f'is_healthy: {is_healthy}')
+    
 
 
 
