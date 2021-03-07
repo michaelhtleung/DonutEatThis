15fa2cc28bb1039dd1536f7055f194fad9bc12d6
Michael <michaelhtleung@gmail.com>
2021-03-07 05:37:06 -0500

    Add ineffective frequentist algorithm

--- test_sql.py
+++ test_sql.py
@@ -14,17 +14,31 @@ cnx = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=
 print("Connected to:", cnx.get_server_info())
 # enter your code here!
 
-# SQL Query 1:
+# # SQL Query 1:
+# cursor = cnx.cursor()
+# query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%s'")
+# cursor.execute(query, ('%dextrose%'))
+# name_list = []
+# for (name) in cursor:
+#     print(name)
+#     name_list.append(name[0])
+# cursor.close()
+
+# SQL Query 2:
 cursor = cnx.cursor()
-query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%s%'")
+query = (f"SELECT DISTINCT c.description FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%s';")
+keywords = ('fat', 'oil', 'sugar', 'salt', 'carbohydrate', 'protein')
+keywords = {k: 0 for k in keywords}
 cursor.execute(query, ('%dextrose%'))
-name_list = []
-for (name) in cursor:
-    print(name)
-    name_list.append(name[0])
+description_list = []
+for description in cursor:
+    print(description)
+    description_list.append(description[0])
+    for key in keywords:
+        if key in description[0]:
+            keywords[key] += 1
+print(keywords)
 cursor.close()
-
-
 
 
 
