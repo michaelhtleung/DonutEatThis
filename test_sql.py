ab46f5f3f44f2a146f8ad27f057c9f21504ced6f
Michael <michaelhtleung@gmail.com>
2021-03-07 07:09:20 -0500

    Add third sql query

--- test_sql.py
+++ test_sql.py
@@ -14,16 +14,16 @@ cnx = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=
 print("Connected to:", cnx.get_server_info())
 search_term = 'dextrose'
 
-# SQL Query 1:
-cursor = cnx.cursor()
-query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%{search_term}%'")
-cursor.execute(query)
-name_list = []
-for (element) in cursor:
-    element = element[0]
-    print(element)
-    name_list.append(element)
-cursor.close()
+# # SQL Query 1:
+# cursor = cnx.cursor()
+# query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%{search_term}%'")
+# cursor.execute(query)
+# name_list = []
+# for (element) in cursor:
+#     element = element[0]
+#     print(element)
+#     name_list.append(element)
+# cursor.close()
 
 # SQL Query 2:
 cursor = cnx.cursor()
@@ -37,6 +37,19 @@ for (element) in cursor:
     if element != 'NULL' and element != 'None' and element != None and len(element.split(' ')[0]) < 20:
         subclass = element
 print(f'subclass: {subclass}')
+cursor.close()
+
+# SQL Query 3:
+cursor = cnx.cursor()
+query = (f"SELECT DISTINCT he.description FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id INNER JOIN compounds_health_effects AS che ON c.id=che.compound_id INNER JOIN health_effects AS he ON he.id=che.health_effect_id WHERE cs.synonym LIKE '%{search_term}%'")
+cursor.execute(query)
+effects = []
+for (element) in cursor:
+    element = element[0]
+    print(element)
+    if element != 'NULL' and element != 'None' and element != None:
+        effects.append(element)
+print(f'effects: {effects}')
 cursor.close()
 
 
