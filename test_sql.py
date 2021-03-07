fe4529b5d79ad9c2578d6d2c669f62224c2620f1
Michael <michaelhtleung@gmail.com>
2021-03-07 07:05:47 -0500

    Add 2 working queries, remove bad frequency alg

--- test_sql.py
+++ test_sql.py
@@ -12,32 +12,31 @@ PASSWORD = "test"
 # connect to MySQL server
 cnx = mysql.connector.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
 print("Connected to:", cnx.get_server_info())
-# enter your code here!
+search_term = 'dextrose'
 
-# # SQL Query 1:
-# cursor = cnx.cursor()
-# query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%s'")
-# cursor.execute(query, ('%dextrose%'))
-# name_list = []
-# for (name) in cursor:
-#     print(name)
-#     name_list.append(name[0])
-# cursor.close()
+# SQL Query 1:
+cursor = cnx.cursor()
+query = (f"SELECT DISTINCT c.name FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%{search_term}%'")
+cursor.execute(query)
+name_list = []
+for (element) in cursor:
+    element = element[0]
+    print(element)
+    name_list.append(element)
+cursor.close()
 
 # SQL Query 2:
 cursor = cnx.cursor()
-query = (f"SELECT DISTINCT c.description FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%s';")
-keywords = ('fat', 'oil', 'sugar', 'salt', 'carbohydrate', 'protein')
-keywords = {k: 0 for k in keywords}
-cursor.execute(query, ('%dextrose%'))
-description_list = []
-for description in cursor:
-    print(description)
-    description_list.append(description[0])
-    for key in keywords:
-        if key in description[0]:
-            keywords[key] += 1
-print(keywords)
+query = (f"SELECT DISTINCT c.subklass FROM compounds AS c INNER JOIN compound_synonyms AS cs ON c.id=cs.source_id WHERE cs.synonym LIKE '%{search_term}%'")
+cursor.execute(query)
+subclass = None
+for (element) in cursor:
+    element = element[0]
+    print(element)
+    # if the first word is 21 letters long, that's probably not the one we want to keep
+    if element != 'NULL' and element != 'None' and element != None and len(element.split(' ')[0]) < 20:
+        subclass = element
+print(f'subclass: {subclass}')
 cursor.close()
 
 
